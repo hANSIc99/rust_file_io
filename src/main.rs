469b929257450b91c3e3b7b0a8d95035eeb6f69a
StephanA <s.avenwedde@gmail.com>
2022-11-17 14:40:36 +0100

    Adds example code for reading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::fs;
-use std::io::prelude::*;
+use std::io::{self, prelude::*};
 
 
 fn main() -> std::io::Result<()> {
+
+    /***********
+     * WRITING *
+     ***********/
 
     /* Simple write */
     fs::write("favorite_websites.txt", b"opensource.com")?;
@@ -14,10 +18,29 @@ fn main() -> std::io::Result<()> {
     /* Write using file descriptor and opening modes */
 
     let mut file = fs::OpenOptions::new()
-                                        //.write(true)
                                         .append(true)
                                         .open("favorite_websites.txt")?;
 
     file.write_all(b"sourceforge.net\n")?;
+
+    /***********
+     * READING *
+     ***********/
+
+    /* Simple read */
+    let websites = fs::read_to_string("favorite_websites.txt")?;
+    println!("Simple read:\n{}\n\n", websites);
+    
+    /* Linewise read */
+    let file = fs::File::open("favorite_websites.txt")?;
+    let lines = io::BufReader::new(file).lines();
+
+    println!("Line wise read:");
+    for line in lines {
+        if let Ok(_line) = line {
+            println!(">>> {}", _line);
+        }
+    }
+
     Ok(())
 }
