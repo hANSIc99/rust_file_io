2260a1b6d6381d892084d924bd5e8bce6e6cf871
StephanA <s.avenwedde@gmail.com>
2022-11-17 13:17:16 +0100

    adds file writing examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io::prelude::*;
+
+
+fn main() -> std::io::Result<()> {
+
+    /* Simple write */
+    fs::write("favorite_websites.txt", b"opensource.com")?;
+
+    /* Write using file descriptor */
+    let mut file = fs::File::create("favorite_websites.txt")?;
+    file.write_all(b"opensource.com\n")?;
+
+    /* Write using file descriptor and opening modes */
+
+    let mut file = fs::OpenOptions::new()
+                                        //.write(true)
+                                        .append(true)
+                                        .open("favorite_websites.txt")?;
+
+    file.write_all(b"sourceforge.net\n")?;
+    Ok(())
 }
